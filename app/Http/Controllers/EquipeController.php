73cbf734b8dced077c455e26d98ec4baaa298c18
adel_djedid <adel_djedid@ens.univ-artois.fr>
2023-12-20 10:29:51 +0100

    fin ticket 4 sans css / images

---
+++ app/Http/Controllers/EquipeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class EquipeController extends Controller
+{
+    public function index(){
+        $equipe= [
+            'nomEquipe'=>"Lucky Seven",
+            'logo'=>"/public/images/logo.jpg",
+            'slogan'=>"....",
+            'localisation'=>"Salle 1X",
+            'membres'=> [
+                [ 'nom'=>"Djedid",'prenom'=>"Adel", 'image'=>"nomFichier", 'fonctions'=>["développer", "concepteur"] ],
+                [ 'nom'=>"Dai",'prenom'=>"Abdelkader", 'image'=>"nomFichier", 'fonctions'=>["développer", "concepteur"] ],
+                [ 'nom'=>"Vanbandon",'prenom'=>"Théo", 'image'=>"nomFichier", 'fonctions'=>["validateur","développer", "concepteur"] ],
+                [ 'nom'=>"Grasso",'prenom'=>"Antoine", 'image'=>"nomFichier", 'fonctions'=>["développer", "concepteur"] ],
+            ],
+            'autres'=>"Autre chose",
+        ];
+        return view('equipes.index', ['equipe' => $equipe]);
+    }
+}
