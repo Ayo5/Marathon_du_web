0aae0576fd7b34b2ec98fbd44a0486dc4df854f3
Vanbandon Théo <theo_vanbandon@ens.univ-artois.fr>
2023-12-21 10:06:32 +0100

    Création du formulaire pour créer un chapitre

---
+++ resources/views/chapitre/create.blade.php
@@ -0,0 +1,33 @@
+<x-layout>
+
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    <form action="{{ route('chapitre.store', $histoire->id) }}" method="post">
+        @csrf
+
+        <label for="histoire_id">ID de l'Histoire</label>
+        <input type="text" name="histoire_id" value="{{ $histoire->id }}" readonly>
+
+        <label for="titre">Titre du Chapitre</label>
+        <input type="text" name="titre" required>
+
+        <label for="titrecourt">Titre Court</label>
+        <input type="text" name="titrecourt" required>
+
+        <label for="texte">Contenu du Chapitre</label>
+        <input type="text" name="texte" rows="5" required>
+
+        <label for="premier">Premier</label>
+        <input type="checkbox" name="premier" value="1">
+
+        <button type="submit">Créer le chapitre</button>
+    </form>
+</x-layout>
