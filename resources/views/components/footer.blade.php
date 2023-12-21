864ded4af3bf79f6253f5e7a2458cc30c429348a
Courbet Loic <loic_courbet@ens.univ-artois.fr>
2023-12-21 01:30:43 +0100

    Commencement du CSS

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,6 +1,18 @@
-<div>
-    Copyright &copy; {{date('Y')}}
-    <a href="http://www.iut-lens.univ-artois.fr/" target="_blank">
-        <strong>IUT Lens - département Info.</strong>
-    </a>
+<div class="footer">
+    <div class="footer-left">
+        <p>The Lucky Seven Company<br/>
+        160 allée du pass plopsa<br/>
+        14652, La tuile<br/>
+        mail : luckyseven.contact@gmail.com<br/>
+        Tel: 03 22 2 2 2 2 2 2</p>
+    </div>
+    <div class="footer-middle">
+    <p>A venir</p>
+    </div>
+    <div class="footer-right">
+        Copyright &copy; {{date('Y')}}<br/>
+        <a href="http://www.iut-lens.univ-artois.fr/" target="_blank">
+            <strong>IUT Lens - département Info.</strong>
+        </a>
+    </div>
 </div>
--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,24 +1,25 @@
 <header class="main-header">
-    <button><a href="{{route('index')}}">🏛 Accueil</a></button>
-    <button><a href="{{route('test-vite')}}">Test Vite</a></button>
-    <button><a href="{{route('contact')}}">☎️ Contact</a></button>
-    <button><a href="{{route('equipes')}}">👨🏻‍💻 Équipes</a></button>
+    <div class="header-logo">
+        <img src="/images/logo_reddrasil_blanc.png" style="width: 10%;" >
+        <a href="{{route('index')}}">Readdrasil</a>
+    </div>
+    <a href="{{route('equipes')}}">Équipe</a>
+    <a href="{{route('test-vite')}}">A propos</a>
+    <a href="{{route('contact')}}">Contact</a>
 
     @auth
-        <button><a href="{{route('home')}}">🏡 Home</a></button>
-        <button><a href="{{route('test-vite')}}">🧪 Test Vite</a></button>
+        <a href="{{route('home')}}">Profil</a>
     @endauth
 
 @guest
-        <button><a href="{{route('register')}}">📥 Enregistrement</a></button>
-        <button><a href="{{route('login')}}">😎 Connexion</a></button>
+        <a href="{{route('register')}}">S'enregistrer </a>
+        <a href="{{route('login')}}">Se connecter</a>
 @endguest
 @auth
     <div class="a-droite">
-        <button><a href="{{route('histoires.create')}}">Nouvelle Histoire</a></button>
+        <a href="{{route('histoires.create')}}" class="header-story">Nouvelle Histoire</a>
         {{Auth::user()->name}}
-        <button><a href="#" id="logout">Logout</a>
-        </button>
+        <a href="#" id="logout">Logout</a>
         <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
             {{ csrf_field() }}
         </form>
@@ -29,3 +30,4 @@
         });
     </script>
 @endauth
+</header>
