acf657ecb2c10050cc18349575a57ff6a22387af
Vanbandon Théo <theo_vanbandon@ens.univ-artois.fr>
2023-12-21 10:01:16 +0100

    Création des fonction create() et store() pour les chapitres

--- app/Http/Controllers/ChapitreController.php
+++ app/Http/Controllers/ChapitreController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Chapitre;
 use App\Models\Histoire;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ChapitreController extends Controller
 {
@@ -21,7 +22,33 @@ class ChapitreController extends Controller
         ]);
     }
 
+    public function create($histoireId) {
+        $histoire = Histoire::findOrFail($histoireId);
 
+        return view('chapitre.create', [
+            'histoire' => $histoire,
+        ]);
+    }
+
+    public function store(Request $request, $histoireId) {
+        $request->validate([
+            'titre' => 'required|string|max:255',
+            'titrecourt' => 'required|string',
+            'texte' => 'required|string',
+        ]);
+
+        $chapitre = new Chapitre([
+            'titre' => $request->input('titre'),
+            'titrecourt' => $request->input('titrecourt'),
+            'texte' => $request->input('texte'),
+            'histoire_id' => $histoireId,
+            'premier' => $request->input('premier', false),
+        ]);
+
+        $chapitre->save();
+
+        return redirect()->route('chapitre.show', [$histoireId, $chapitre->id]);
+    }
 
 
 }
